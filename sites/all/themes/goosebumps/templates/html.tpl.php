d192913596f6bbf7d66293b66adae66ddafa0858
jjhudson <mail@johnhudson.net>
2016-07-04 07:36:38 -0700

    Ass backups

--- sites/all/themes/goosebumps/templates/html.tpl.php
+++ sites/all/themes/goosebumps/templates/html.tpl.php
@@ -24,21 +24,25 @@
         print $page; 
     ?>
     
-    <script src="<?= base_path() . drupal_get_path('theme', 'goosebumps'); ?>/js/main.js"></script>
+    <script src="<?= base_path() . drupal_get_path('theme', 'goosebumps'); ?>/js/main.min.js" async></script>
 
-    <script>
-
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-46878903-6', 'auto');
-
-        // ADD A SECONDARY CLIENT ANALYTCIS CODE LIKE THIS
-        //ga('create', 'UA-XXXXXXXX-X', 'auto', {'name': 'CLIENTS NAME'});
+    <?php if (!isset($_SERVER['HTTP_USER_AGENT']) || stripos($_SERVER['HTTP_USER_AGENT'], 'Speed Insights') === false): ?>
         
-        ga('send', 'pageview');
-        ga('meylercampbell.send', 'pageview');
+        <script>
 
-    </script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-46878903-6', 'auto');
+
+            // ADD A SECONDARY CLIENT ANALYTCIS CODE LIKE THIS
+            //ga('create', 'UA-XXXXXXXX-X', 'auto', {'name': 'CLIENTS NAME'});
+            
+            ga('send', 'pageview');
+            ga('meylercampbell.send', 'pageview');
+
+        </script>
+
+    <?php endif; ?>
 
 </body>
-</html>
+</h
